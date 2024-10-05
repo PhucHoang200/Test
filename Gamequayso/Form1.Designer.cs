ed31653b8718fee78f01aa3880891361095cb380
PhucHoang200 <phucphuc2004444@gmail.com>
2024-10-05 23:12:14 +0700

    Test

---
+++ Gamequayso/Form1.Designer.cs
@@ -0,0 +1,193 @@
+﻿namespace Gamequayso
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.Label label4;
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.timer3 = new System.Windows.Forms.Timer(this.components);
+            label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label4
+            // 
+            label4.Location = new System.Drawing.Point(43, 240);
+            label4.Name = "label4";
+            label4.Size = new System.Drawing.Size(100, 23);
+            label4.TabIndex = 3;
+            label4.Text = "Tiền người chơi";
+            // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.label1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label1.Location = new System.Drawing.Point(43, 43);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(100, 167);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "7";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.label2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(233, 43);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(102, 167);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "7";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.label3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(426, 43);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(100, 167);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "7";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(173, 241);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(353, 22);
+            this.textBox1.TabIndex = 4;
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(37, 292);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(106, 40);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Quay số";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(236, 292);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(106, 40);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "Stop";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(429, 292);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(97, 40);
+            this.button3.TabIndex = 7;
+            this.button3.Text = "Game mới";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(236, 380);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(106, 43);
+            this.button4.TabIndex = 8;
+            this.button4.Text = "Kết thúc";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // timer2
+            // 
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // timer3
+            // 
+            this.timer3.Tick += new System.EventHandler(this.timer3_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(569, 450);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.Timer timer3;
+    }
+}
+
---
+++ Gamequayso/Form1.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlTypes;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Gamequayso
+{
+    public partial class Form1 : Form
+    {
+        Random random = new Random();
+        Timer timerStop = new Timer();
+        public Form1()
+        {
+            InitializeComponent();
+
+            timerStop.Interval = 5000; 
+            timerStop.Tick += new EventHandler(timerStop_Tick);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+           
+            timer1.Start();
+            timer2.Start();
+            timer3.Start();
+
+            timerStop.Start();
+            if (int.Parse(textBox1.Text) < 20)
+            {
+                MessageBox.Show("Số tiền hiện tại nhỏ hơn 20, vui lòng nạp thêm xu để tiếp tục chơi!", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            textBox1.Text = (int.Parse(textBox1.Text) - 20).ToString();
+            timer1_Tick(sender, e);
+            timer2_Tick(sender, e);
+            timer3_Tick(sender, e);
+
+        }
+
+        private void timerStop_Tick(object sender, EventArgs e)
+        {
+            timer1.Stop();
+            timer2.Stop();
+            timer3.Stop();
+            timerStop.Stop();
+
+
+        
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "100";
+            label1.Text = "";
+            label2.Text = "";
+            label3.Text = "";
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(textBox1.Text))
+            {
+                textBox1.Text = "100";
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            int randomNumber = random.Next(0, 8);
+            label1.Text = randomNumber.ToString();
+
+           
+
+            int currentValue;
+            if (int.TryParse(textBox1.Text, out currentValue))
+            {
+                if (randomNumber == 7)
+                {
+                    currentValue += 30;
+                }
+                textBox1.text = currentValue.ToString();
+
+                
+
+                
+            }
+        }
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            int randomNumber = random.Next(0, 9);
+            label2.Text = randomNumber.ToString();
+
+            if (randomNumber == 7)
+            {
+                int currentValue;
+                if (int.TryParse(textBox1.Text, out currentValue))
+                {
+                    currentValue += 40;
+                    
+                }
+               
+              
+            }
+        }
+
+        private void timer3_Tick(object sender, EventArgs e)
+        {
+            int randomNumber = random.Next(0, 9);
+            label3.Text = randomNumber.ToString();
+
+            if (randomNumber == 7)
+            {
+                int currentValue;
+                if (int.TryParse(textBox1.Text, out currentValue))
+                {
+                    currentValue += 50;
+                    
+                }
+               
+                
+            }
+        }
+    }
+}
